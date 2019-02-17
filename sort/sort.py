e1e30b104fff32fd8743e7ac53a255a2ef61f450
Sophia Sherman <sherman205@gmail.com>
2019-02-17 18:13:56 -0500

    Add main function and methodize logic

--- sort/sort.py
+++ sort/sort.py
@@ -1,28 +1,35 @@
 import csv
 
-input_list = []
-input_file = open("sort/input.csv",'r')
-# keep track of number of lines in input.csv
-count = 0
-for line in input_file.readlines():
-    count = count + 1
-    if count > 1:
-        print("oh no! input.csv should only contain a single line to read")
-        input_list.clear()
-    else:
-        actual_line = line.rstrip('\n')
-        input_list = actual_line.split(',')
+def sort_csv(src, dest):
+    input_list = []
+    input_file = open(src,'r')
+    # keep track of number of lines in input.csv
+    count = 0
+    for line in input_file.readlines():
+        count = count + 1
+        if count > 1:
+            print("oh no! input.csv should only contain a single line to read")
+            input_list.clear()
+        else:
+            actual_line = line.rstrip('\n')
+            input_list = actual_line.split(',')
 
-input_file.close()
+    input_file.close()
 
-# Sorting should ignore case
-input_list.sort(key=lambda i: i.upper(), reverse=True)
+    # Sorting should ignore case
+    input_list.sort(key=lambda i: i.upper(), reverse=True)
 
-with open("sort/output.csv",'w') as output_file:
-    wr = csv.writer(output_file, quoting=csv.QUOTE_NONE)
-    # especially takes care of an empty input.csv
-    result = [i for i in input_list if i]
-    if not result:
-        wr.writerow([])
-    else:
-        wr.writerow(input_list)
+    with open(dest,'w') as output_file:
+        wr = csv.writer(output_file, quoting=csv.QUOTE_NONE)
+        # especially takes care of an empty input.csv
+        result = [i for i in input_list if i]
+        if not result:
+            wr.writerow([])
+        else:
+            wr.writerow(input_list)
+
+if __name__ == "__main__":
+    try:
+        sort_csv("sort/input.csv","sort/output.csv")
+    except Exception as e:
+        print("oops you have an error here")
